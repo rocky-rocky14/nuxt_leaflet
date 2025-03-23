b8e6d97b3102717f1e4014114de95ccc069512b3
rocky <b201521115@gmail.com>
2025-03-23 22:37:02 +0900

    環境設定の追加。 ・devcontainer ・vscode拡張機能共有準備

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <p>Welcome to the home page</p>
+        <v-btn @click="click">クリック</v-btn>
+    </div>
+</template>
+<script setup lang="ts">
+const click = () =>{
+    alert('クリックされました')
+}
+</script>
